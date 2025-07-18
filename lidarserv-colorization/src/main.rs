7f04f4e611cb05b829890e898a6b6c945f639694
n1c01 <36988840+n1c01@users.noreply.github.com>
2025-07-18 11:02:19 +0200

    Add todos

--- lidarserv-colorization/src/main.rs
+++ lidarserv-colorization/src/main.rs
@@ -26,18 +26,27 @@ fn main() -> ExitCode {
     }
 }
 
-fn run(args: AppOptions){
+fn run(args: AppOptions) -> Result<(),String>{
+    //install signal handler
+    
     //ROS connection Thread
+    //TODO: ROS Thread, that reads out the images and the positions of the camera
     
     //Processing frustum Thread
+    //TODO: Processing Thread, that processes the camera position and calculates the frustum
     
     //LidarServ query Thread
+    //TODO: LidarServ Thread, that queries the frustum to retrieve the points from the lidarserv server
     
     //LidarServ answer Thread
+    //TODO: LidarServ Thread, that recieves the points from the lidarserv server and sends them to the colorization thread
     
     //Processing colorization Thread
+    //TODO: Processing Thread, that colorizes the points
     //init_colorize();
     
     //LidarServ save Thread
-
+    //TODO: LidarServ Thread, that sends the processed points to the lidarserv server
+    
+    return Ok(());
 }
