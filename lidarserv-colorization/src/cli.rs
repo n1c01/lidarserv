9a0dd6b7baa3f8aed40f48968862083145680ab9
n1c01 <36988840+n1c01@users.noreply.github.com>
2025-07-18 10:52:16 +0200

    added commandline interface

---
+++ lidarserv-colorization/src/cli.rs
@@ -0,0 +1,47 @@
+use std::str::FromStr;
+
+use anyhow::anyhow;
+use clap::Parser;
+
+/// Connector that forwards point clouds from ROS into lidarserv.
+///
+/// The connection to ROS should be established automatically if you are within the ROS environment. If the connection fails, you can set the following ROS connection options:
+///
+///  - The master URL:
+///     via command line: `__master:=http://localhost:11311/`
+///     via environment variable: `ROS_MASTER_URI=http://localhost:11311/`
+///  - The ros hostname or ip address:
+///     via command line: `__hostname:=localhost` or `__ip:=127.0.0.1`
+///     via environment variables: `ROS_HOSTNAME=localhost` or `ROS_IP=127.0.0.1`
+///  - The ros namespace:
+///     via command line: `__ns:=my_namespace`
+///     via environment variable: `ROS_NAMESPACE=my_namespace`
+///  - The node name:
+///     via command line: `__name:=lidarserv`
+#[derive(Debug, Parser, Clone)]
+#[command(verbatim_doc_comment)]
+pub struct AppOptions {
+    /// Verbosity of the command line output.
+    #[clap(long, default_value = "info")]
+    pub log_level: log::Level,
+    
+    /// Vector of the ROS topics where the image messages will be published to.
+    #[clap(long, default_values = &["/Cam1_Image","/Cam2_Image","/Cam3_Image"])]
+    pub image_topics: Vec<String>,
+
+    /// Name of the fixed coordinate frame that the lidar points will be
+    /// transformed to before sending to the lidarserv server.
+    // note: The default should probably be "map" according to REP-105 https://www.ros.org/reps/rep-0105.html
+    #[clap(long, default_value = "camera_init")]
+    pub world_frame: String,
+
+    /// Hostname of the lidarserv server
+    #[clap(long, default_value = "::0")]
+    pub host: String,
+
+    /// Port of the lidarserv server
+    #[clap(long, default_value = "4567")]
+    pub port: u16,
+    
+}
+
--- lidarserv-colorization/src/main.rs
+++ lidarserv-colorization/src/main.rs
@@ -1,15 +1,32 @@
+use std::process::ExitCode;
+use clap::Parser;
+use cli::AppOptions;
+use rosrust::api::resolve::get_unused_args;
+use log::{debug, error, info};
+
+mod cli;
 mod point_cloud_colorizer;
 mod init_colorize;
 
-use crate::point_cloud_colorizer::PointCloudColorizer;
-use crate::init_colorize::init_colorize;
 
-use las::point::Format;
-use las::{Builder, Reader, Writer};
-use std::io::BufWriter;
-use std::path::Path;
+fn main() -> ExitCode {
+    // arg parsing
+    let args = AppOptions::parse_from(get_unused_args());
 
-fn main() {
+    // logger
+    simple_logger::init_with_level(args.log_level).unwrap();
+
+    // run
+    match run(args) {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(e) => {
+            error!("Error: {}", e);
+            ExitCode::FAILURE
+        }
+    }
+}
+
+fn run(args: AppOptions){
     //ROS connection Thread
     
     //Processing frustum Thread
