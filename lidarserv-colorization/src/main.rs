e354fd4a464ccaae4aa720750784c2a0558a90dc
n1c01 <36988840+n1c01@users.noreply.github.com>
2025-09-13 14:07:38 +0200

    renumbering threads

--- lidarserv-colorization/src/main.rs
+++ lidarserv-colorization/src/main.rs
@@ -46,10 +46,10 @@ fn run(args: AppOptions) -> Result<(), Error> {
     let stop_token_status = stop_source_status.clone();
     let stop_source = stop_source_status.child_token();
     let stop_source2 = stop_source.clone();
+    let child_token1 = stop_source.child_token();
     let child_token2 = stop_source.child_token();
     let child_token3 = stop_source.child_token();
     let child_token4 = stop_source.child_token();
-    let child_token5 = stop_source.child_token();
 
     let (exit_tx, exit_rx) = channel();
     {
@@ -72,16 +72,16 @@ fn run(args: AppOptions) -> Result<(), Error> {
     let (commands_tx, commands_rx) = mpsc::channel();
     let (image_data_tx, image_data_rx) = mpsc::channel(); //Channel for the image data of the camera.
     let status = Arc::new(Status::default());
-    let exit_tx1 = exit_tx.clone();
-    let status1 = Arc::clone(&status);
+    let exit_tx0 = exit_tx.clone();
+    let status0 = Arc::clone(&status);
     let join_ros = {
-        let args1 = args.clone();
+        let args0 = args.clone();
         thread::spawn(move || {
-            status1.t0_ros_thread_running.store(true, Ordering::Relaxed);
-            thread_0_ros(args1, commands_rx, image_data_tx, status1.clone()).log_error();
-            status1.t0_ros_thread_running.store(false, Ordering::Relaxed);
+            status0.t0_ros_thread_running.store(true, Ordering::Relaxed);
+            thread_0_ros(args0, commands_rx, image_data_tx, status0.clone()).log_error();
+            status0.t0_ros_thread_running.store(false, Ordering::Relaxed);
             debug!("ros_thread: sending exit message");
-            exit_tx1.send(()).ok()
+            exit_tx0.send(()).ok()
         })
     };
 
@@ -89,113 +89,113 @@ fn run(args: AppOptions) -> Result<(), Error> {
     //Processing Thread, that processes the camera position and calculates the frustum
     let (image_id_and_frustum_data_tx, image_id_and_frustum_data_rx) = mpsc::channel();
     let (image_data_bypass_tx, image_data_bypass_rx) = mpsc::channel(); //Channel for the image data of the camera.
-    let exit_tx2 = exit_tx.clone();
-    let status2 = Arc::clone(&status);
-    let args2 = args.clone();
+    let exit_tx1 = exit_tx.clone();
+    let status1 = Arc::clone(&status);
+    let args1 = args.clone();
 
     let join_processing_frustum = {
         thread::spawn(move || {
-            status2
+            status1
                 .t1_process_frustum_thread_running
                 .store(true, Ordering::Relaxed);
             thread_1_process_frustum(
-                args2,
-                child_token2,
+                args1,
+                child_token1,
                 image_data_rx,
                 image_data_bypass_tx,
                 image_id_and_frustum_data_tx,
-                status2.clone(),
+                status1.clone(),
             )
             .log_error();
-            status2
+            status1
                 .t1_process_frustum_thread_running
                 .store(false, Ordering::Relaxed);
             debug!("process_frustum_thread: sending exit message");
-            exit_tx2.send(()).ok()
+            exit_tx1.send(()).ok()
         })
     };
 
     //LidarServ query Thread, that queries the frustum to retrieve the points from the lidarserv server
     let (points_tx, points_rx) = mpsc::channel(); //todo!(rename more precise)
-    let exit_tx3 = exit_tx.clone();
-    let status3 = Arc::clone(&status);
-    let args3 = args.clone();
+    let exit_tx2 = exit_tx.clone();
+    let status2 = Arc::clone(&status);
+    let args2 = args.clone();
     let join_lidarserv_query = {
         thread::spawn(move || {
             let rt = Runtime::new().unwrap();
-            status3
+            status2
                 .t2_send_frustum_thread_running
                 .store(true, Ordering::Relaxed);
             rt.block_on(thread_2_send_frustum(
-                args3,
-                child_token3,
+                args2,
+                child_token2,
                 image_id_and_frustum_data_rx,
                 points_tx,
-                status3.clone(),
+                status2.clone(),
             ))
             .log_error();
-            status3
+            status2
                 .t2_send_frustum_thread_running
                 .store(false, Ordering::Relaxed);
             debug!("send_frustum_thread: sending exit message");
-            exit_tx3.send(()).ok()
+            exit_tx2.send(()).ok()
         })
     };
 
     //LidarServ answer Thread, that recieves the points from the lidarserv server and sends them to the colorization thread
     let (colorization_data_tx, colorization_data_rx) = mpsc::channel();
-    let exit_tx4 = exit_tx.clone();
-    let status4 = Arc::clone(&status);
-    let args4 = args.clone();
+    let exit_tx3 = exit_tx.clone();
+    let status3 = Arc::clone(&status);
+    let args3 = args.clone();
     let join_lidarserv_answer = {
         thread::spawn(move || {
-            status4
+            status3
                 .t3_collect_colorization_data_thread_running
                 .store(true, Ordering::Relaxed);
             thread_3_collect_colorization_data(
-                args4,
-                child_token4,
+                args3,
+                child_token3,
                 points_rx,
                 image_data_bypass_rx,
                 colorization_data_tx,
-                status4.clone(),
+                status3.clone(),
             )
             .log_error();
-            status4
+            status3
                 .t3_collect_colorization_data_thread_running
                 .store(false, Ordering::Relaxed);
 
             debug!("collect_colorization_data_thread: sending exit message");
-            exit_tx4.send(()).ok()
+            exit_tx3.send(()).ok()
         })
     };
 
     //Processing colorization Thread
-    let exit_tx5 = exit_tx.clone();
-    let status5 = Arc::clone(&status);
-    let args5 = args.clone();
+    let exit_tx4 = exit_tx.clone();
+    let status4 = Arc::clone(&status);
+    let args4 = args.clone();
     let (colorized_data_tx,colorized_data_rx) = mpsc::channel();
 
     let join_colorization = {
         thread::spawn(move || {
-            status5
+            status4
                 .t4_managing_colorization_thread_running
                 .store(true, Ordering::Relaxed);
             //Processing colorization Thread, that colorizes the points
             debug!("managing_colorization_thread: start thread");
             thread_4_managing_colorization(
-                args5,
-                child_token5,
+                args4,
+                child_token4,
                 colorization_data_rx,
                 colorized_data_tx,
-                status5.clone(),
+                status4.clone(),
             )
             .log_error();
-            status5
+            status4
                 .t4_managing_colorization_thread_running
                 .store(false, Ordering::Relaxed);
             debug!("managing_colorization_thread: sending exit message");
-            exit_tx5.send(()).ok()
+            exit_tx4.send(()).ok()
         })
     };
 
